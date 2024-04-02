4bd3f29a251d27c2dabdea89115c2e9e0abf0d12
meeranh <themeeranhassan@gmail.com>
2024-04-02 10:33:00 +0530

    Removed protobuf files from clangd LSP

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -3,10 +3,14 @@
 require('lspconfig').pyright.setup({})				-- Python
 require("lspconfig").lua_ls.setup({})					-- Lua
 require("lspconfig").jdtls.setup({})					-- Java
-require("lspconfig").clangd.setup({})					-- C/C++
 require("lspconfig").gopls.setup({})					-- Go
 require("lspconfig").tsserver.setup({})				-- JavaScript/TypeScript
 require("lspconfig").rust_analyzer.setup({})	-- Rust
 require("lspconfig").cssls.setup({})					-- CSS
 require("lspconfig").html.setup({})						-- HTML
 require("lspconfig").phpactor.setup({})				-- PHP
+
+-- C/C++
+require("lspconfig").clangd.setup({
+	filetypes = { "c", "cpp", "objc", "objcpp", "cuda" }
+})
